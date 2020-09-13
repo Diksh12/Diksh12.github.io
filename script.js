1e63f6fc2e6faf9cea61e1d1cbeaea5949b390f1
Dikshita Pusam <dikshitapusam.dp@gmail.com>
2020-09-13 17:48:00 +0530

    newly updated

---
+++ script.js
@@ -0,0 +1,4 @@
+const closeLightbox=document.querySelector(".close.lightbox");
+closeLightbox.addEventListener("click",function(){
+    document.querySelector(".lightbox").getElementsByClassName.display="none";
+})
